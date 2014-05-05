7569cbeb90592d520b8b37958c2cecb27737c65e
Kendall Carey <kendall.carey@gmail.com>
2014-05-04 22:32:46 -0700

    projects!

---
+++ unit2_projects/die_1.rb
@@ -0,0 +1,47 @@
+# U2.W5: Die Class 1: Numeric
+
+
+# I worked on this challenge Kendall Carey and Tim Howard.
+
+# 2. Pseudocode
+
+# Input: the number of sides when you call the method
+# Output: a random number between 1 and the number of sides that were input
+# Steps: 
+
+create a class, define the die method, define .sides and .roll
+
+
+# 3. Initial Solution
+
+class Die
+  def initialize(sides)
+    # code goes here
+  end
+  
+  def sides
+    # code goes here
+  end
+  
+  def roll
+    # code goes here
+  end
+end
+
+
+
+# 4. Refactored Solution
+
+
+
+
+
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+
+
+
+
+
+
+# 5. Reflection 
